2926cbc2515f13c71c4f4ab31047496bdf9d35cf
odajunpei <i.penju1022@gmail.com>
2021-08-27 14:51:57 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,9 +24,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
-
 
 
   def update
